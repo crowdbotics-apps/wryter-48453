fb2b4b9ad44f8eb4d7195f703aaa6f268bf6dfd1
Crowdbotics <team@crowdbotics.com>
2024-06-17 05:54:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wryter-48453",
+  url: "https://wryter-48453.botics.co",
+  api: "https://wryter-48453.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
